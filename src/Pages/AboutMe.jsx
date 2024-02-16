ad407f90bb67d813e146cc6c5dc5b872b1257e5a
Shaurya Gupta <ShauryaGupta118@gmail.com>
2024-02-17 03:26:22 +0530

    removing get in touch

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -22,18 +22,17 @@ const Gallery = () => {
 
 With a well-established practice for home physiotherapy, he is an expert in treatment of conditions such as back pain, neck pain, knee pain, sciatica, cervical spondylosis, cervicocephalic syndrome, cervicobrachial syndrome, disc bulge, frozen shoulder, arthritis, osteoporosis, muscle weakness, balancing problems… Post surgery rehab for shoulder, knee surgery / TKR, hip replacement, ligament reconstruction. Neurological issues like stroke, nerve injuries, Bell's palsy, multiple sclerosis, Parkinson's.</p>
                     {/* <!-- Add your physiotherapy content here --> */}
-                    
+{/*                     
                     <div className="contact-component">
                     <div className='gallerytxtheading'>Get in Touch</div>
                     <div className='logos'>
-                    {/* <a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Instagram.svg/2048px-Instagram.svg.png" className="logoinabout"/></a> */}
+                    <a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Instagram.svg/2048px-Instagram.svg.png" className="logoinabout"/></a>
                     <a href="mailto:jaiphysio103@gmail.com"><img src="https://www.svgrepo.com/show/14478/email.svg" className="logoinabout"/></a>
                     <a href="tel:+917355177359"><img src="https://www.svgrepo.com/show/5236/phone-call.svg" className="logoinabout"/></a>
-                    {/* <a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg" className="logoinabout"/></a> */}
+                    <a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg" className="logoinabout"/></a>
                     </div>
-                    {/* <!-- Add your social media icons and contact info here --> */}
-                    </div>
-                    </div>
+                    </div>*/}
+                    </div> 
                     
                     <img src={image} alt="Image" className='image-component'/>
                     {/* <!-- Add your image content here --> */}
